f2791485790f00b0235064c8f92b5ec8f077c5f0
Nguyễn Trí Toán <111261368+tritoan24@users.noreply.github.com>
2025-06-20 18:02:49 +0700

    test

--- gallery-master/vite.config.ts
+++ gallery-master/vite.config.ts
@@ -1,35 +1,26 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import autoprefixer from "autoprefixer";
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        view: resolve(__dirname, "view.html"),
-      },
-    },
-  },
-  server: {
-    host: "0.0.0.0",
-  },
-  css: {
-    postcss: {
-      plugins: [
-        autoprefixer({
-          overrideBrowserslist: [
-            "Android 4.1",
-            "iOS 7.1",
-            "Chrome > 31",
-            "ff > 31",
-            "ie >= 8",
-            "> 1%",
-          ],
-        }),
-      ],
-    },
-  },
+	base: "/",
+	server: {
+		host: "0.0.0.0"
+	},
+	css: {
+		postcss: {
+			plugins: [
+				autoprefixer({
+					overrideBrowserslist: [
+						"Android 4.1",
+						"iOS 7.1",
+						"Chrome > 31",
+						"ff > 31",
+						"ie >= 8",
+						"> 1%",
+					]
+				})
+			]
+		}
+	}
 });
